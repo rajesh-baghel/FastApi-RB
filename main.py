da6c06c4ac9bd5cba44c45d1d648b457a2f85e1b
yogesh baghel <yashbaghel613@gmail.com>
2023-06-04 00:52:37 +0530

    creating routers for api endpoints

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 from fastapi import FastAPI, Path, Query
-from pydantic import BaseModel
+from api import courses, sections, users
 
 app = FastAPI(
     title="FastAPI RB",
@@ -15,23 +15,9 @@ app = FastAPI(
     }
 )
 
-users = []
+app.include_router(courses.router)
+app.include_router(sections.router)
+app.include_router(users.router)
 
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
 
-@app.get("/users", response_model= List[User])
-async def get_users():
-    return users
 
-@app.post("/users")
-async def create_users(user: User):
-    users.append(user)
-    return "Success"
-
-@app.get("/users/{id}")
-async def get_user(id: int = Path(..., description="The ID of the user you want to retrieve", gt=1),
-                   q: str = Query(None, max_length= 4)):
-    return {"User": users[id],"query": q}
